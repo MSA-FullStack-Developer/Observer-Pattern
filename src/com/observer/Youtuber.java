51e217b34e737df10f5db7adbec705a6a6989118
Jinho Song <sth4881@naver.com>
2022-02-14 16:04:57 +0900

    [Update] 🔥 옵저버 패턴 코드 및 이미지 업데이트

--- src/com/observer/Youtuber.java
+++ src/com/observer/Youtuber.java
@@ -9,31 +9,23 @@ public class Youtuber implements Subject {
 
 	public Youtuber(String channelName) {
 		this.channelName = channelName;
-		System.out.println("유튜버 "+ this.channelName + "님께서 채널을 개설하였습니다.");
+		this.numberOfSubscribers = 0;
 	}
-
-	public void showInfo() {
-		this.numberOfSubscribers = observers.size();
-		System.out.println(channelName + "님 채널의 현재 구독자 수는 " + numberOfSubscribers + "명 입니다.");
-	}
-
+	
 	public void videoUpload(Video video) {
-//		System.out.println(channelName + "님! [" + video.getVideoName() + "] 영상이 업로드 되었습니다!");
 		notifySubscribers(video);
 	}
 
 	@Override
 	public void subscribe(Observer observer) {
+		this.numberOfSubscribers += 1;
 		observers.add(observer);
-		String name = observer.getName();
-		System.out.println(name + "님이 구독했습니다.");
 	}
 
 	@Override
 	public void unsubscribe(Observer observer) {
+		this.numberOfSubscribers -= 1;
 		observers.remove(observer);
-		String name = observer.getName();
-		System.out.println(name + "님이 구독을 취소했습니다.");
 	}
 
 	@Override
